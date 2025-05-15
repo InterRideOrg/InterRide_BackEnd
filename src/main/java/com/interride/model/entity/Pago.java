85e25192b457142f83e525b68af290f0670b3f68
nathan <ecoaguilaf@gmail.com>
2025-05-15 15:46:16 -0500

    feat: se agrego la tabla de pagos

---
+++ src/main/java/com/interride/model/entity/Pago.java
@@ -0,0 +1,44 @@
+package com.interride.model.entity;
+
+
+import com.interride.model.enums.EstadoPago;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "pago")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Pago {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "estado", nullable = false)
+    EstadoPago estado;
+
+    @Column(name = "fecha_hora_pago", nullable = false)
+    private LocalDateTime fechaHoraPago;
+
+    @Column(name = "monto", nullable = false)
+    private Double monto;
+
+    @ManyToOne
+    @JoinColumn(name = "pasajero_id", referencedColumnName = "id", foreignKey = @ForeignKey(name = "FK_pago_pasajero"))
+    private Pasajero pasajero;
+
+    @ManyToOne
+    @JoinColumn(name = "conductor_id", referencedColumnName = "id", foreignKey = @ForeignKey(name = "FK_pago_conductor"))
+    private Conductor conductor;
+
+    @ManyToOne
+    @JoinColumn(name = "tarjeta_id", referencedColumnName = "id", foreignKey = @ForeignKey(name = "FK_pago_tarjeta"))
+    private Viaje viaje;
+}
