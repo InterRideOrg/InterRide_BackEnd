bbf7f653a29a9c2176d5f171f36ff35f6348b292
JoeTurpo <joeturpo@gmail.com>
2025-06-07 02:59:07 -0500

    feat: Servicios para la funcionalidad de la HU16 de esta rama

--- src/main/java/com/interride/service/ConductorService.java
+++ src/main/java/com/interride/service/ConductorService.java
@@ -1,8 +1,10 @@
 package com.interride.service;
 
 import com.interride.dto.request.ConductorRegistroRequest;
+import com.interride.dto.response.ConductorPerfilPublicoResponse;
 import com.interride.dto.response.ConductorRegistroResponse;
 
 public interface ConductorService {
     ConductorRegistroResponse registrarConductor(ConductorRegistroRequest request);
+    ConductorPerfilPublicoResponse obtenerPerfilConductorAsignado(Integer idViaje);
 }
--- src/main/java/com/interride/service/Impl/ConductorServiceImpl.java
+++ src/main/java/com/interride/service/Impl/ConductorServiceImpl.java
@@ -1,9 +1,15 @@
 package com.interride.service.Impl;
 
 import com.interride.dto.request.ConductorRegistroRequest;
+import com.interride.dto.response.ConductorPerfilPublicoResponse;
 import com.interride.dto.response.ConductorRegistroResponse;
+import com.interride.exception.BusinessRuleException;
+import com.interride.exception.ResourceNotFoundException;
+import com.interride.mapper.ConductorMapper;
 import com.interride.model.entity.Conductor;
+import com.interride.model.entity.Viaje;
 import com.interride.repository.ConductorRepository;
+import com.interride.repository.ViajeRepository;
 import com.interride.service.ConductorService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -17,6 +23,7 @@ public class ConductorServiceImpl implements ConductorService {
 
     private final ConductorRepository conductorRepository;
     private final PasswordEncoder passwordEncoder;
+    private final ViajeRepository viajeRepository;
 
     @Override
     public ConductorRegistroResponse registrarConductor(ConductorRegistroRequest request) {
@@ -46,5 +53,18 @@ public class ConductorServiceImpl implements ConductorService {
 
 
         return new ConductorRegistroResponse("Registro exitoso. Se le ha enviado un correo de confirmación.");
+    }
+
+
+    @Override
+    public ConductorPerfilPublicoResponse obtenerPerfilConductorAsignado(Integer idViaje) {
+        Viaje viaje = viajeRepository.findById(idViaje)
+                .orElseThrow(() -> new ResourceNotFoundException("Viaje con ID " + idViaje + " no encontrado"));
+
+        if (viaje.getConductor() == null) {
+            throw new BusinessRuleException("Ningun conductor ha aceptado aún realizar este viaje.");
+        }
+
+        return ConductorMapper.toResponse(viaje.getConductor());
     }
 }
