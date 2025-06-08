47fffba90130ba949a3ebeff41a67bb9617e1db8
JoeTurpo <joeturpo@gmail.com>
2025-06-07 19:43:19 -0500

    syle: Se eliminaron importaciones que no se usaban ni tampoco aportaban a la funcionalidad de algun metodo

--- src/main/java/com/interride/repository/PasajeroViajeRepository.java
+++ src/main/java/com/interride/repository/PasajeroViajeRepository.java
@@ -1,14 +1,11 @@
 package com.interride.repository;
 
-import com.interride.model.entity.Calificacion;
 import com.interride.model.entity.PasajeroViaje;
-import com.interride.model.entity.Ubicacion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface PasajeroViajeRepository extends JpaRepository<PasajeroViaje, Integer> {
 
