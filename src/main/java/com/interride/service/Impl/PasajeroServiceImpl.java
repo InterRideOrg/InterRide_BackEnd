20a79628bb2b8c2c1151a441a0e027d7a87de2e6
JoeTurpo <joeturpo@gmail.com>
2025-06-20 22:27:13 -0500

    feat: Se volvio a integrar funcionalidades

--- src/main/java/com/interride/service/Impl/PasajeroServiceImpl.java
+++ src/main/java/com/interride/service/Impl/PasajeroServiceImpl.java
@@ -1,6 +1,9 @@
 package com.interride.service.Impl;
 
 
+import com.interride.dto.response.PasajeroPerfilPublicoResponse;
+import com.interride.exception.ResourceNotFoundException;
+import com.interride.mapper.PasajeroMapper;
 import com.interride.model.entity.Pasajero;
 import com.interride.repository.PasajeroRepository;
 import com.interride.service.PasajeroService;
@@ -8,20 +11,29 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-/**
- * Alta de pasajeros (HU-01) - ahora deja la contraseña
- * cifrada antes de persistir y de mapearla de vuelta a DTO.
- */
 @Service
 @RequiredArgsConstructor
 public class PasajeroServiceImpl implements PasajeroService {
 
     private final PasajeroRepository pasajeroRepository;
+    private final PasajeroMapper pasajeroMapper;
 
     @Override
     @Transactional(readOnly = true)
     public Pasajero getById(Integer id) {
         return pasajeroRepository.findById(id).orElseThrow();
     }
+
+
+    @Transactional
+    @Override
+    public PasajeroPerfilPublicoResponse obtenerPerfilPasajero(Integer idPasajero) {
+        Pasajero pasajero = pasajeroRepository.findById(idPasajero)
+                .orElseThrow(() -> new ResourceNotFoundException("Pasajero con ID " + idPasajero + " no encontrado."));
+
+        return pasajeroMapper.toPublicProfileDTO(pasajero);
+    }
+
+
 
 }
